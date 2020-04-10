81bd96a6d35f65ce8e3d9454f5596422d5a056af
Artem Filippov <xamber-zenit@mail.ru>
2020-04-10 12:45:41 +0000

    Remove unused code

--- internal/web/server.go
+++ internal/web/server.go
@@ -5,31 +5,10 @@ import (
 	"encoding/json"
 	log "github.com/sirupsen/logrus"
 	"github.com/xamber/saturn/internal/database"
-	"github.com/xamber/saturn/internal/entity"
-	"html/template"
 	"net/http"
 )
 
-func GetBySourceName(w http.ResponseWriter, r *http.Request) {
-	logger := log.WithField("Handler", "GetBySourceName")
-	logger.Info("Get request handled")
-
-	limit := 50
-
-	name := r.URL.Query().Get("name")
-
-	articles, err := database.GetByName(name, limit)
-	if err != nil {
-		w.WriteHeader(500)
-	}
-
-	err = json.NewEncoder(w).Encode(articles)
-	if err != nil {
-		w.WriteHeader(500)
-	}
-}
-
-func GetBySourceGroup(w http.ResponseWriter, r *http.Request) {
+func GetArticlesByTopic(w http.ResponseWriter, r *http.Request) {
 	logger := log.WithField("Handler", "GetBySourceName")
 	logger.Info("Get request handled")
 
@@ -48,44 +27,8 @@ func GetBySourceGroup(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type IndexData struct {
-	Company    []entity.Article
-	HackerNews []entity.Article
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	logger := log.WithField("Handler", "Index")
-
-	tmpl := template.Must(template.ParseFiles("layout.html"))
-
-	complany, err := database.GetByGroup("Компании", 50)
-	if err != nil {
-		logger.Error(err)
-		w.WriteHeader(500)
-	}
-
-	hackerNews, err := database.GetByName("Hacker News: Newest", 50)
-	if err != nil {
-		logger.Error(err)
-		w.WriteHeader(500)
-	}
-
-	data := IndexData{
-		Company:    complany,
-		HackerNews: hackerNews,
-	}
-
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		logger.Error(err)
-		w.WriteHeader(500)
-	}
-}
-
 func Serve(ctx context.Context) {
-	http.HandleFunc("/", Index)
-	http.HandleFunc("/name", GetBySourceName)
-	http.HandleFunc("/group", GetBySourceGroup)
+	http.HandleFunc("/topic", GetArticlesByTopic)
 
 	err := http.ListenAndServe(":3000", nil)
 	log.Error(err)
