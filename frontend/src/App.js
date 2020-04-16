b155f90473705d4810937adc11ee2accfbf984c9
Artem Filippov <xamber-zenit@mail.ru>
2020-04-16 16:09:38 +0000

    Source list and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import {Divider, Logo, Main, Sidebar, Footer} from "./components/Layout/layout";
 import MenuItem from "./components/MenuItem/menuItem";
-import {Divider, Logo, Main, Sidebar} from "./components/Layout/layout";
 import ArticleList from "./components/ArticleList/articleList";
 import SourceList from "./components/SourceList/SourceList";
 
@@ -17,6 +17,22 @@ import russiaIcon from "./assets/Russia.svg";
 import workIcon from "./assets/work.svg";
 import peopleIcon from "./assets/people.svg";
 import vikingIcon from "./assets/viking.svg";
+
+
+const About = () => <div className={"about"}>
+    <p>2020 &nbsp;
+    Created by Artem Filippov.&nbsp;
+    </p>
+    <p>
+    Inspired by&nbsp;
+    <a href="https://infomate.club/">infomate.club</a> from&nbsp;
+    <a href="https://vas3k.ru/">vas3k</a>
+    </p>
+    <p>
+    Sourse code is available on my Github: <a href="https://github.com/Xamber/saturn">Saturn</a>&nbsp;
+    LinkedIn: <a href="https://www.linkedin.com/in/artem-filippov-spb/">artem-filippov-spb</a> &nbsp;
+    </p>
+</div>;
 
 
 let topics = [
@@ -40,6 +56,7 @@ class App extends React.Component {
         this.state = {
             active: topics[0].name,
             articles: [],
+            sources: [],
         };
     }
 
@@ -57,12 +74,30 @@ class App extends React.Component {
             );
     }
 
+    fetchSources() {
+        let api = "/sources?name=";
+        fetch(api + this.state.active)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({sources: result});
+                },
+                (error) => {
+                    console.error(error)
+                }
+            );
+    }
+
     changeTopic(topicName) {
-        this.setState({active: topicName}, this.fetchArticles);
+        this.setState({active: topicName}, () => {
+            this.fetchArticles();
+            this.fetchSources();
+        });
     }
 
     componentDidMount() {
-        this.fetchArticles(topics[0].name)
+        let initialTopic = topics[0].name;
+        this.changeTopic(initialTopic);
     }
 
     render() {
@@ -71,21 +106,26 @@ class App extends React.Component {
         for (let item of topics) {
             let menuItem = <MenuItem key={item.name} active={this.state.active === item.name} name={item.name}
                                      img={item.img}
-                                     onClick={() => { this.changeTopic(item.name) }}/>;
+                                     onClick={() => {
+                                         this.changeTopic(item.name)
+                                     }}/>;
             Menu.push(menuItem);
         }
 
         return (
             <div className="app">
                 <Sidebar>
-                    <Logo/>
+                    <Logo />
                     {Menu}
                 </Sidebar>
                 <Main>
                     <ArticleList articlies={this.state.articles}/>
                     <Divider maxWidth={"10%"}/>
-                    <SourceList name={this.state.active}/>
+                    <SourceList sources={this.state.sources}/>
                 </Main>
+                <Footer>
+                    <About />
+                </Footer>
             </div>
         );
     }
--- frontend/src/components/SourceList/SourceList.js
+++ frontend/src/components/SourceList/SourceList.js
@@ -6,36 +6,9 @@ const Source = (props) => {
     return <a href={props.WebSite}><Icon link={props.WebSite} /> {props.Name}</a>
 };
 
-class SourceList extends React.Component {
-    api = "/sources?name=";
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            sources: [], // TODO do it without state
-        };
-    }
-
-    componentDidMount(props) {
-        fetch(this.api + this.props.name)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState(
-                        {sources: result}
-                    );
-                },
-                (error) => {
-                    console.error(error)
-                }
-            );
-    }
-
-    render() {
-        let sources = this.state.sources.map((source) => <Source key={source.Id} {...source} />);
-        return <div className={"source-list"}>{sources}</div>
-    }
-}
+const SourceList = (props) => {
+    let sources = props.sources.map((source) => <Source key={source.Id} {...source} />);
+    return <div className={"source-list"}>{sources}</div>
+};
 
 export default SourceList;
